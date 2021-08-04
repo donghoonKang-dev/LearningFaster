e3244ad04876696447e03330053db4358e5947a0
donghoonKang-dev <donghoon.dev@gmail.com>
2021-08-04 10:44:52 +0900

    7. useState를 통해 컴포넌트에서 ...

--- vlpt-modern-react/src/App.js
+++ vlpt-modern-react/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="choco" color="red" isSpecial="true" />
-      <Hello />
-    </Wrapper>
+    <Counter />
   );
 }
 
