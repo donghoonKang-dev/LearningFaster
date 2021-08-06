94fbb2320761f6fff7158b13bc7a4f65bb0255ec
donghoonKang-dev <donghoon.dev@gmail.com>
2021-08-06 10:17:24 +0900

    21. 커스텀 Hooks

--- vlpt-modern-react/src/CreateUser.js
+++ vlpt-modern-react/src/CreateUser.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CreateUser = ({ username, email, onChange, onCreate }) => {
   return (
@@ -17,7 +17,7 @@ const CreateUser = ({ username, email, onChange, onCreate }) => {
       />
       <button onClick={onCreate}>등록</button>
     </div>
-  )
+  );
 }
 
 export default React.memo(CreateUser);
