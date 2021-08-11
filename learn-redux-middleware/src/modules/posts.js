810c2e589c1d274f7fdb3d4405e318cfe4245e78
donghoonKang-dev <donghoon.dev@gmail.com>
2021-08-11 10:25:10 +0900

    7-6. API 재로딩 문제 해결하기

--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,22 +1,25 @@
 import * as postsAPI from '../api/posts';
-import { createPromiseThunk, reducerUtils, handleAsyncActions } from '../lib/asyncUtils';
+import {
+  createPromiseThunk,
+  reducerUtils,
+  handleAsyncActions,
+  createPromiseThunkById,
+  handleAsyncActionsById
+} from '../lib/asyncUtils';
 
-/* 액션 타입 */
-//포스트 여러개 조회하기
-const GET_POSTS = 'GET_POSTS'; //요청 시작
-const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; //요청 성공
-const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; //요청 실패
-//포스트 하나 조회하기
+const GET_POSTS = 'GET_POSTS';
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR';
 const GET_POST = 'GET_POST';
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
 const GET_POST_ERROR = 'GET_POST_ERROR';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial()
+  post: {}
 };
 
 export default function posts(state = initialState, action) {
@@ -24,11 +27,11 @@ export default function posts(state = initialState, action) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      return handleAsyncActions(GET_POSTS, 'posts')(state, action);
+      return handleAsyncActions(GET_POSTS, 'posts', true)(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action);
+      return handleAsyncActionsById(GET_POST, 'post')(state, action);
     default:
       return state;
   }
