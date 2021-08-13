657a16e7ee69e2bc35178ed86b4d775d0fca654f
donghoonKang-dev <donghoon.dev@gmail.com>
2021-08-13 21:23:32 +0900

    8-3. 타입스크립트로 리액트 상태 관리하기

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Greetings from './Greetings';
+import ReducerSample from './ReducerSample';
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`)
-  }
-  return <Greetings name="Hello" onClick={onClick} />;
-}
+  return <ReducerSample />;
+};
 
 export default App;
