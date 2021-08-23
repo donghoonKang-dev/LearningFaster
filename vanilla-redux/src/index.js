74e882da29265cfea9e79e6f87f4a3edc45d045b
donghoonKang-dev <donghoon.dev@gmail.com>
2021-08-23 15:41:30 +0900

    vanilla-redux: #2 PURE REDUX: TO DO LIST

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,35 +1,72 @@
 import { createStore } from 'redux';
 
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const number = document.getElementById('number');
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const ul = document.getElementById('todo-list');
 
-const PLUS = 'PLUS';
-const MINUS = 'MINUS';
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
 
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case MINUS:
-      return count - 1;
-    case PLUS:
-      return count + 1;
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== action.id);
     default:
-      return count
+      return state;
+  };
+};
+
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
+const dispatchAddTodo = text => {
+  store.dispatch(addTodo(text));
+};
+const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+};
+
+const paintTodos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach(toDo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.type = 'button';
+    btn.addEventListener('click', dispatchDeleteTodo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+const store = createStore(reducer);
+store.subscribe(paintTodos);
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  if (toDo === '') {
+    alert('내용을 입력해야지');
+  } else {
+    input.value = '';
+    dispatchAddTodo(toDo);
   }
 }
 
-const countStore = createStore(countModifier);
-
-const countMinus = () => {
-  countStore.dispatch({ type: MINUS });
-}
-const countPlus = () => {
-  countStore.dispatch({ type: PLUS });
-}
-const countChange = () => {
-  number.innerText = countStore.getState();
-}
-
-countStore.subscribe(countChange);
-minus.addEventListener('click', countMinus);
-plus.addEventListener('click', countPlus);
+form.addEventListener("submit", onSubmit);
